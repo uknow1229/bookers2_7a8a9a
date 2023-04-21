e7e496c82e876fdf213543691be4648bc05bfa12
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-04-21 01:22:26 +0000

    [Start]7a

---
+++ app/models/book.rb
@@ -0,0 +1,25 @@
+class Book < ApplicationRecord
+  belongs_to :user
+	validates :title,presence:true
+	validates :body,presence:true,length:{maximum:200}
+
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+
+  def favorited_by?(user)
+   favorites.exists?(user_id: user.id)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index,:show,:edit,:update]
+  get '/search', to: 'searches#search'
+end
