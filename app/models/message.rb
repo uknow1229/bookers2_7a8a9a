f91393e572350b581e500547dc69c1342d473721
saya <uknow.love1229@gmail.com>
2023-04-25 15:58:33 +0900

    [Fix]bootstrap

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   belongs_to :room
 
   validates :message, presence: true, length: { maximum: 140 }
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
